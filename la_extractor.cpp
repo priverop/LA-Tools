d00b305640cdb177d0c9981b67d7b5a5ace95a87
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-08-22 03:20:23 +0200

    Added header and footer to extract

--- la_extractor.cpp
+++ la_extractor.cpp
@@ -1,12 +1,18 @@
 #include "HexTools.h"
+#include "BinaryTools.h"
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <sstream>
+#include <iomanip>
 #include <sys/stat.h>
 
 int main(int argc, char* argv[]){
 
   HexTools tools;
+  BinaryTools bTools;
+
+  const string FOOTER = "454E4454000000000000000000000000";
 
   if(argc < 2){
     cout << "Not enough arguments." << endl;
@@ -15,18 +21,19 @@ int main(int argc, char* argv[]){
   else{
     string inputFileName = argv[1];
     ifstream inputFile;
-    ifstream::pos_type size;
-    char * memblock;
+    uint32_t size;
+    const string folderName = "./extract";
+    unsigned int block_number = 0;
+    uint32_t offset = 0x20; // Nos saltamos los 32bytes de la cabecera (0x20 = 32)
+    string fileName;
 
     inputFile.open(inputFileName, ios::in | ios::binary | ios::ate);
 
       if (inputFile.is_open())
       {
         size = inputFile.tellg();
-        memblock = new char [size];
-
-        unsigned int block_number = 0;
-        uint32_t offset = 0x20; // Nos saltamos los 32bytes de la cabecera
+        cout << "== LUMINOUS ARC EXTRACTOR by Nex ==" << endl << endl;
+        cout << "Iniciando la extracción..." << endl << endl;
 
         // Guardamos los primeros 4bytes tras la tabla de punteros para tomarlo como fin de bucle
         uint32_t first_pointer;
@@ -37,28 +44,38 @@ int main(int argc, char* argv[]){
         inputFile.seekg(first_pointer, ios::beg);
         inputFile.read (reinterpret_cast<char *>(&end_table), sizeof(end_table));
 
-        const string folderName = "./extract";
-
         if (mkdir(folderName.c_str(), S_IRWXU | S_IRWXG | S_IRWXO) != 0)
         {
           cout << "Error creating extract folder" << endl;
           cout << "Please delete the folder" << endl;
         }
         else{
-          cout << "== LUMINOUS ARC EXTRACTOR by Nex ==" << endl << endl;
-          cout << "Iniciando la extracción..." << endl << endl;
+
+          // Guardamos la cabecera del fichero
+          char header[offset];
+          inputFile.seekg(0, ios::beg);
+          inputFile.read(header, offset);
+          fileName = "Header";
+
+          bTools.createBinaryFile(folderName + "/" + to_string(block_number) + " - " + fileName,
+          string(header, offset));
+
+          block_number++;
+
+          uint32_t pointer_beg_block;
+          uint32_t block_size;
 
           while(offset != first_pointer){
 
             // Almacenamos los 8 bytes del puntero del inicio del bloque
-            uint32_t pointer_beg_block;
+
             inputFile.seekg(offset, ios::beg);
             inputFile.read (reinterpret_cast<char *>(&pointer_beg_block), sizeof(pointer_beg_block));
 
             offset += sizeof(pointer_beg_block);
 
             // Almacenamos los 8 bytes del tamaño de bloque
-            uint32_t block_size;
+
             inputFile.seekg(offset, ios::beg);
             inputFile.read (reinterpret_cast<char *>(&block_size), sizeof(block_size));
 
@@ -69,18 +86,30 @@ int main(int argc, char* argv[]){
             inputFile.seekg(pointer_beg_block, ios::beg);
             inputFile.read (block, block_size);
             // Nombre del archivo
-            string fileName = string(block, sizeof(uint32_t));
+            fileName = string(block, sizeof(uint32_t));
 
             // Guardamos bloque en fichero
-            std::ofstream outfile (folderName + "/" + fileName + to_string(block_number));
-            outfile << string(block, block_size);
             cout << "Extrayendo bloque número " << to_string(block_number) << "..." << endl;
-            cout << fileName << to_string(block_number) << " creado correctamente." << endl << endl;
-            outfile.close();
+
+            bTools.createBinaryFile(folderName + "/" + to_string(block_number) + " - " + fileName,
+            string(block, block_size));
 
             block_number++;
 
           } // while
+
+          // Guardamos el footer del fichero
+          uint32_t pointer_beg_footer = pointer_beg_block+block_size;
+          uint32_t footer_size = size - pointer_beg_footer;
+
+          char footer[footer_size];
+          inputFile.seekg(pointer_beg_footer, ios::beg);
+          inputFile.read(footer, footer_size);
+          fileName = "Footer";
+
+          bTools.createBinaryFile(folderName + "/" + to_string(block_number) + " - " + fileName,
+          string(footer, footer_size));
+
         } // if mkdir
       } // if inputFile is_open
   } // if arguments correct
