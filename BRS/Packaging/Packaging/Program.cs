ae45ae45a8a300d1e7498164b46b793ead6c7316
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-08-25 23:15:26 +0200

    Visual Studio project for BRS

---
+++ BRS/Packaging/Packaging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Packaging
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
